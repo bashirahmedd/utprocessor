22854df4e2170782ba1be6eae170d61180fd4451
bashir <bashir.ahmedd@gmail.com>
2022-02-07 02:09:03 +0500

    updated code

--- node_process_med/app.js
+++ node_process_med/app.js
@@ -1,3 +1,29 @@
-const fs = require('fs')
+//'use strict';
 
-console.log('hello node');
+const fs = require('fs');
+
+function jsonReader(filePath, cb) {
+    
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try {
+      const object = JSON.parse(fileData);
+      return cb && cb(null, object);
+    } catch (err) {
+      return cb && cb(err);
+    }
+  });
+}
+
+jsonReader("./input_data.json", (err, customer) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  //console.log(customer.address); // => "Infinity Loop Drive"
+  console.log(customer.div.div[0].p[0].a['#text'])
+});
+
+//console.log('hello node');
