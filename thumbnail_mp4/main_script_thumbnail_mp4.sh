2ed646fcf4841f2a9d2da185287a0ace5d65babd
bashir <bashir.ahmedd@gmail.com>
2022-08-16 15:22:15 +0500

    error check for cmds added

--- thumbnail_mp4/main_script_thumbnail_mp4.sh
+++ thumbnail_mp4/main_script_thumbnail_mp4.sh
@@ -3,25 +3,29 @@
 
 #set -euo pipefail 
 
-base_files_dir='/home/naji/bashir-workspace/kids_corner/420p/usb1/read_ladybird'
-target_dir='/home/naji/bashir-workspace/kids_corner/420p/usb1/read_ladybird/'
-slp_val="$((2+2))"       #in sec
+base_files_dir='/home/naji/bashir-workspace/kids_corner/420p/usb1/read_0_ladybird_1_2'
+target_dir='/home/naji/bashir-workspace/kids_corner/420p/usb1/read_0_ladybird_1_2/'
+let slp_val="2+2"       #in sec
 
 while read -r full_name; do
    
     file_name=$(basename "$full_name")
     vid_len=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$full_name"`
-    vid_len1=`echo "$vid_len/1 - 2" | bc`
-     
-    #video start and end thumbnail 
-    sleep $slp_val
-    ffmpeg -i "$full_name" -ss 00:00:02 -frames:v 1 -y "$target_dir""$file_name"".start.jpeg"
-    sleep $slp_val
-    ffmpeg -ss "$vid_len1" -i "$full_name" -ss 00:00:01 -frames:v 1 -y "$target_dir""$file_name"".end.jpeg"
-    sleep $slp_val
 
-    echo "$vid_len"
-    echo "$vid_len1"
+    if [ $? -eq 0 ]; then
+        vid_len1=`echo "$vid_len/1 - 2" | bc`
+        #video start and end thumbnail 
+        sleep $slp_val
+        ffmpeg -i "$full_name" -ss 00:00:02 -frames:v 1 -y "$target_dir""$file_name"".start.jpeg"
+
+        if [ $? -eq 0 ]; then
+            sleep $slp_val
+            ffmpeg -ss "$vid_len1" -i "$full_name" -ss 00:00:01 -frames:v 1 -y "$target_dir""$file_name"".end.jpeg"
+            sleep $slp_val
+        fi
+    fi
+    # echo "$vid_len"
+    # echo "$vid_len1"
     echo "$full_name"
     # break
 done <<< "`find  $base_files_dir -type f`"
