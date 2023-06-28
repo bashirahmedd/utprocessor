852fb2bf46130e5aae8dd90afed111a18ddd64de
bashir <bashir.ahmedd@gmail.com>
2023-06-28 20:41:34 +0500

    minor improvement info

--- playlists_url_download/util/prepare_id_from_json.sh
+++ playlists_url_download/util/prepare_id_from_json.sh
@@ -4,8 +4,12 @@
 # remove qoutes from  id string 
 
 src_dir="../output/"
-src_nme="PLF_XMn-l0vPS5vLAaL1f73z2HwSNZ1vZy"
+src_nme="PLg6KfZlgBuDXwQ56IZ8tByQGgmgONP4Wr"
 src_file="$src_dir""$src_nme"".json"
-
+out_file="$src_dir""$src_nme"".txt"
 #up limit in seconds
-jq -r '.[]|select(.duration<600)|.id' "$src_file" > "$src_dir""$src_nme"".txt"   
+jq -r '.[]|select(.duration<600)|.id' "$src_file" > "$out_file"   
+
+json_obj_count=`jq '. | length' $src_file`
+ids_count=`cat $out_file|wc -l`
+echo "Ids filtered = ""$ids_count"" from ""$json_obj_count"
